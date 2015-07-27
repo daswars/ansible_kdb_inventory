5be67fcfd370de56caf9c464c227fb52f2dba3cc
escoreal <escoreal@gmx.de>
2015-07-27 17:53:45 +0200

    group_vars to host_vars, tags to groups

--- kdb_inventory.py
+++ kdb_inventory.py
@@ -5,69 +5,69 @@ sys.setdefaultencoding('utf8')
 import libkeepass
 import os
 import json
-#import xml.etree.cElementTree as ET
 import lxml.etree as ET
-    
+from string import maketrans
+
 def kdb_inventory():
   filename = os.environ["KDB_PATH"]
   credentials = { 'password' : os.environ["KDB_PASS"] }
-  hosts_info = {}
+  hosts = {}
   inventory = {}
   inventory_vars = {}
   with libkeepass.open(filename, **credentials) as kdb:
     xmldata = ET.fromstring(kdb.pretty_print())
-    for group in xmldata.findall(".//Group"):
-      group_name = str(group.find("./Name").text)
-      group_name = group_name.lower()
-      for parent in group:
-        parents = {
-          dept.find('Name').text
-          for dept in parent.xpath('ancestor::Group')        
-        }
-      group_vars = []
-      hosts = []
-      for entry in group.findall("./Entry"):
-        vars = {}
-        hostname = ""
-        for string in entry.findall("./String"):
-          host_entry = string.find("[Key='Title']")
-          if host_entry is not None:
-            host = string.find("./Value")
-            hostname = str(host.text).lower()
-            if hostname in hosts:
-              break
-            if hostname != "group_vars":
-              hosts.append(hostname)
-          key = string.find("./Key")
-          key = str(key.text).lower()
-          value = string.findtext("./Value")
-          if value and value != hostname and key != "title":
-            vars[key] = str(value)
-        if hostname == "group_vars":
-          group_vars = vars
+    for history in xmldata.xpath(".//History"):
+      history.getparent().remove(history)      
+    for entry in xmldata.findall(".//Entry"):
+      hostvars = {}
+      hostname = None
+      for string in entry.findall("./String"):
+        key   = string.findtext("./Key").lower()
+        value = string.findtext("./Value")
+        if key == 'title' and ' ' not in value and value != 'group_vars':
+          hostname = value.lower()
+        if value and key != "title":
+          hostvars[key] = value
+      if hostname and hostname != "group_vars" and ' ' not in hostname:
+        for ancestor in entry.iterancestors("Group"):
           try:
-            inventory[group_name]["vars"] = group_vars
+            for aentry in ancestor.find("./Entry/String[Key='Title'][Value='group_vars']"):              
+              aentry = aentry.getparent().getparent()
+              for astring in aentry.findall("./String"):
+                avalue = astring.findtext("./Value")
+                if not avalue:
+                  continue
+                akey = astring.findtext("./Key").lower()
+                if not hostvars.has_key(akey) and akey != "title":
+                  hostvars[akey] = avalue
+          except TypeError:
+            continue
+        groups = {
+          group.find('Name')
+          for group in entry.xpath('ancestor::Group')
+        }
+        for group in groups:
+          group = group.text.lower()
+          try:
+            inventory[group]["hosts"].append(hostname)
           except KeyError:
-            inventory[group_name] = {}
-            inventory[group_name]["vars"] = group_vars
-        elif hostname and ' ' not in hostname:
-          hosts_info[hostname] = vars
-        if hostname and hostname != "group_vars" and ' ' not in hostname:
-          for actual_group in parents:
-            actual_group = actual_group.lower()
+            inventory[group] = {}
+            inventory[group]["hosts"] = [hostname]
+        tags = entry.findtext("./Tags").split(';')
+        for tag in tags:
+          if tag:
+            tag = tag.translate(maketrans('=','_'))
+            tag = "tag_" + tag
             try:
-              if hostname not in inventory[actual_group]["hosts"]:
-                inventory[actual_group]["hosts"].append(hostname)
+              inventory[tag]["hosts"].append(hostname)
             except KeyError:
-              try:
-                inventory[actual_group]["hosts"] = [hostname]
-              except KeyError:              
-                inventory[actual_group] = {}
-                inventory[actual_group]["hosts"] = [hostname]
-            inventory[actual_group]["hosts"].sort()
-  inventory_vars["hostvars"] = hosts_info
-  inventory["_meta"] = inventory_vars
-  print json.dumps(inventory, indent=2, sort_keys=True)
+              inventory[tag] = {}
+              inventory[tag]["hosts"] = [hostname]
+      if hostname:
+        hosts[hostname] = hostvars
+      inventory_vars["hostvars"] = hosts
+      inventory["_meta"] = inventory_vars
+    print json.dumps(inventory, indent=2, sort_keys=True)
 
 if __name__ == '__main__':
   try:
